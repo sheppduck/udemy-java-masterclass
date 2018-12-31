76ef0f44b752e8bc78187399da3ea62a4f372616
joel_sheppard <joel.sheppard@vmturbo.com>
2018-12-31 14:44:47 -0500

    wrapping up lecture 18 variables

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,21 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, Joel!!");
-        int myfirstnumber = 5;
-        System.out.println(myfirstnumber);;
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myChallenge = 1000 - myTotal;
+
+        System.out.println(myFirstNumber) ;
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+
+        System.out.println(myTotal);
+
+        System.out.println(myChallenge);
     }
 }
