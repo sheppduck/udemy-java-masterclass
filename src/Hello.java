0642a3538ed679a0c235b227b7be5ad6ef5dcca4
joel_sheppard <joel.sheppard@vmturbo.com>
2018-12-31 14:26:14 -0500

    first vars, yay (again)

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, Joel!!");
+        int myfirstnumber = 5;
+        System.out.println(myfirstnumber);;
     }
 }
