c3b09040f3fa71a82be6bf733cccee4100b38d34
joel_sheppard <joel.sheppard@vmturbo.com>
2019-01-24 11:48:13 -0500

    next commit

---
+++ IfKeywordAndCodeBlocks/src/com/joel/Main.java
@@ -0,0 +1,31 @@
+package com.joel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        /*if (score == 5000) {
+            System.out.println("Your score was 5000!!");
+            System.out.println("This was executed!");
+        }
+*/
+        if (score < 4000 && score > 1000) {
+            System.out.println("Your score was less than 5000 - but greater than 1000!!");
+        }
+        else if(score < 1000) {
+            System.out.println("Your score was less than 1000, sorry!");
+
+        }
+        else {
+            System.out.println("Got here...");
+        }
+
+
+
+
+    }
+}
