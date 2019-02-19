f508d0f9a65b2c4b50049bce72ea235175e4b208
joel_sheppard <joel.sheppard@vmturbo.com>
2019-02-19 09:58:23 -0500

    With Challenge - IDK if it's 'right' buy I certainly get -1 to print..If wrong I'll commit with corrections

--- methods_lecture28/src/com/joel/Main.java
+++ methods_lecture28/src/com/joel/Main.java
@@ -4,19 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        calculateScore(false, 800, 5, 100);
 
         calculateScore(true, 10000, 8, 200);
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("From calculateScore: Your final score was " + finalScore);
+            return finalScore;
+        }   else {
+            int youLose = -1;
+            System.out.println("If we got here: " + youLose);
+            return youLose;
         }
+
 
 
     }
@@ -26,6 +32,8 @@ public class Main {
 //       ##################################
 //       Challenge
 //       ##################################
-
+//       how would you return a -1 if gameOver was set to false?
 
 }
+
+
